1a14e9f4d214adc93fe7bcdceb55118d8d60f8dc
Hunter Larson <hslarson@umail.ucsb.edu>
2023-08-28 21:16:02 -0700

    Pushed previous changes

--- src/SparkFun_Qwiic_Humidity_AHT20.cpp
+++ src/SparkFun_Qwiic_Humidity_AHT20.cpp
@@ -1,264 +1,248 @@
-/****************************************************************
- * SparkFun_Qwiic_Humidity_AHT20.cpp
- * SparkFun Qwiic Humidity AHT20 Library Source File
- * Priyanka Makin @ SparkFun Electronics
- * Original Creation Date: March 25, 2020
- * https://github.com/sparkfun/SparkFun_Qwiic_Humidity_AHT20_Arduino_Library
- * 
- * Pick up a board here: https://www.sparkfun.com/products/16618
- * 
- * This file implements all the functions of the AHT20 class.
- * Functions in this library can return the humidity and temperature 
- * measured by the sensor. All data is communicated over I2C bus.
- * 
- * Development environment specifics:
- *  IDE: Arduino 1.8.9
- *  Hardware Platform: Arduino Uno
- *  Qwiic Humidity AHT20 Version: 1.0.0
- * 
- * This code is lemonade-ware; if you see me (or any other SparkFun
- * employee) at the local, and you've found our code helpful, please
- * buy us a round!
- * 
- * Distributed as-is; no warranty is given.
- ***************************************************************/
-
 #include <SparkFun_Qwiic_Humidity_AHT20.h>
 
-/*--------------------------- Device Status ------------------------------*/
-bool AHT20::begin(TwoWire &wirePort)
-{
-    _i2cPort = &wirePort; //Grab the port the user wants to communicate on
+// Constructor
+AHT20::AHT20(const uint8_t deviceAddress) { _deviceAddress = deviceAddress; }
 
-    _deviceAddress = AHT20_DEFAULT_ADDRESS; //We had hoped the AHT20 would support two addresses but it doesn't seem to
+// Initialize and calibrate the module
+// Read delay enforces a minumum delay between measurements (2 seconds recommended)
+bool AHT20::begin(TwoWire* wire, uint32_t read_delay_ms) {
+  _Wire = wire;
 
-    if (isConnected() == false)
-        return false;
+  if (!isConnected()) return false;
 
-    //Wait 40 ms after power-on before reading temp or humidity. Datasheet pg 8
-    delay(40);
+  // Wait 40 ms after power-on before reading temp or humidity.
+  delay(40);
 
-    //Check if the calibrated bit is set. If not, init the sensor.
-    if (isCalibrated() == false)
-    {
-        //Send 0xBE0800
-        initialize();
+  // Check if the calibrated bit is set. If not, init the sensor.
+  if (!isCalibrated()) {
+    // Initialize module
+    initialize();
 
-        //Immediately trigger a measurement. Send 0xAC3300
-        triggerMeasurement();
-
-        delay(75); //Wait for measurement to complete
-
-        uint8_t counter = 0;
-        while (isBusy())
-        {
-            delay(1);
-            if (counter++ > 100)
-                return (false); //Give up after 100ms
-        }
-
-        //This calibration sequence is not completely proven. It's not clear how and when the cal bit clears
-        //This seems to work but it's not easily testable
-        if (isCalibrated() == false)
-        {
-            return (false);
-        }
-    }
-
-    //Check that the cal bit has been set
-    if (isCalibrated() == false)
-        return false;
-
-    //Mark all datums as fresh (not read before)
-    sensorQueried.temperature = true;
-    sensorQueried.humidity = true;
-
-    return true;
-}
-
-//Ping the AHT20's I2C address
-//If we get a response, we are correctly communicating with the AHT20
-bool AHT20::isConnected()
-{
-    _i2cPort->beginTransmission(_deviceAddress);
-    if (_i2cPort->endTransmission() == 0)
-        return true;
-
-    //If IC failed to respond, give it 20ms more for Power On Startup
-    //Datasheet pg 7
-    delay(20);
-
-    _i2cPort->beginTransmission(_deviceAddress);
-    if (_i2cPort->endTransmission() == 0)
-        return true;
-
-    return false;
-}
-
-/*------------------------ Measurement Helpers ---------------------------*/
-
-uint8_t AHT20::getStatus()
-{
-    _i2cPort->requestFrom(_deviceAddress, (uint8_t)1);
-    if (_i2cPort->available())
-        return (_i2cPort->read());
-    return (0);
-}
-
-//Returns the state of the cal bit in the status byte
-bool AHT20::isCalibrated()
-{
-    return (getStatus() & (1 << 3));
-}
-
-//Returns the state of the busy bit in the status byte
-bool AHT20::isBusy()
-{
-    return (getStatus() & (1 << 7));
-}
-
-bool AHT20::initialize()
-{
-    _i2cPort->beginTransmission(_deviceAddress);
-    _i2cPort->write(sfe_aht20_reg_initialize);
-    _i2cPort->write((uint8_t)0x08);
-    _i2cPort->write((uint8_t)0x00);
-    if (_i2cPort->endTransmission() == 0)
-        return true;
-    return false;
-}
-
-bool AHT20::triggerMeasurement()
-{
-    _i2cPort->beginTransmission(_deviceAddress);
-    _i2cPort->write(sfe_aht20_reg_measure);
-    _i2cPort->write((uint8_t)0x33);
-    _i2cPort->write((uint8_t)0x00);
-    if (_i2cPort->endTransmission() == 0)
-        return true;
-    return false;
-}
-
-//Loads the
-void AHT20::readData()
-{
-    //Clear previous data
-    sensorData.temperature = 0;
-    sensorData.humidity = 0;
-
-    if (_i2cPort->requestFrom(_deviceAddress, (uint8_t)6) > 0)
-    {
-        _i2cPort->read(); // Read and discard state
-
-        uint32_t incoming = 0;
-        incoming |= (uint32_t)_i2cPort->read() << (8 * 2);
-        incoming |= (uint32_t)_i2cPort->read() << (8 * 1);
-        uint8_t midByte = _i2cPort->read();
-
-        incoming |= midByte;
-        sensorData.humidity = incoming >> 4;
-
-        sensorData.temperature = (uint32_t)midByte << (8 * 2);
-        sensorData.temperature |= (uint32_t)_i2cPort->read() << (8 * 1);
-        sensorData.temperature |= (uint32_t)_i2cPort->read() << (8 * 0);
-
-        //Need to get rid of data in bits > 20
-        sensorData.temperature = sensorData.temperature & ~(0xFFF00000);
-
-        //Mark data as fresh
-        sensorQueried.temperature = false;
-        sensorQueried.humidity = false;
-    }
-}
-
-//Triggers a measurement if one has not been previously started, then returns false
-//If measurement has been started, checks to see if complete.
-//If not complete, returns false
-//If complete, readData(), mark measurement as not started, return true
-bool AHT20::available()
-{
-    if (measurementStarted == false)
-    {
-        triggerMeasurement();
-        measurementStarted = true;
-        return (false);
-    }
-
-    if (isBusy() == true)
-    {
-        return (false);
-    }
-
+    // Start a measurement
+    _read_delay = 0;  // Force reading
     readData();
-    measurementStarted = false;
-    return (true);
+
+    // Verify calibration
+    if (!isCalibrated()) {
+      return false;
+    }
+  }
+
+  // Set read delay
+  _read_delay = read_delay_ms;
+
+  // Mark all datums as old (need to be refreshed)
+  sensorQueried.temperature = true;
+  sensorQueried.humidity = true;
+
+  return true;
 }
 
-bool AHT20::softReset()
-{
-    _i2cPort->beginTransmission(_deviceAddress);
-    _i2cPort->write(sfe_aht20_reg_reset);
-    if (_i2cPort->endTransmission() == 0)
-        return true;
+// Verify I2C communication
+bool AHT20::isConnected() {
+  // Just performing a null transmission messes up the sensor
+  // Instead, read the status
+  uint8_t status = getStatus();
+  if (status) {
+    return true;
+  }
+
+  // If IC failed to respond, give it 20ms more for Power On Startup
+  delay(100);
+  return getStatus() != 0;
+}
+
+// Returns the status byte
+uint8_t AHT20::getStatus() {
+  _Wire->requestFrom(_deviceAddress, (uint8_t)1);
+  if (_Wire->available()==1) return (_Wire->read());
+  return 0;
+}
+
+// Returns the state of the cal bit in the status byte
+bool AHT20::isCalibrated() { return (getStatus() & (1 << 3)); }
+
+// Returns the state of the busy bit in the status byte
+bool AHT20::isBusy() { return (getStatus() & (1 << 7)); }
+
+// Send command to initialize the sensor
+bool AHT20::initialize() {
+  _Wire->beginTransmission(_deviceAddress);
+  _Wire->write(CMD_INITIALIZE_2);
+  _Wire->write(CMD_INITIALIZE_1);
+  _Wire->write(CMD_INITIALIZE_0);
+  return _Wire->endTransmission() == 0;
+}
+
+// Reset the device
+bool AHT20::softReset() {
+  _Wire->beginTransmission(_deviceAddress);
+  _Wire->write(CMD_RESET);
+  return _Wire->endTransmission() == 0;
+}
+
+// Send command to start measurement
+bool AHT20::triggerMeasurement() {
+  // Check for ongoing measurement
+  if (_measurementStarted) return true;
+
+  // Enforce read delay
+  if (millis() - _last_read < _read_delay) {
     return false;
+  }
+
+  _Wire->beginTransmission(_deviceAddress);
+  _Wire->write(CMD_MEASURE_2);
+  _Wire->write(CMD_MEASURE_1);
+  _Wire->write(CMD_MEASURE_0);
+  if (_Wire->endTransmission() == 0) {
+    _measurementStarted = true;
+    return true;
+  }
+
+  return false;
 }
 
-/*------------------------- Make Measurements ----------------------------*/
+// Reads new data from sensor
+// Ignores sensorQueried flags
+bool AHT20::readData() {
+  // Start measurement
+  // Return false if reading cooldown condition isn't met
+  if (!_measurementStarted) {
+    if (triggerMeasurement()) {
+      delay(75);
+    } else
+      return false;
+  }
 
-float AHT20::getTemperature()
-{
-    if (sensorQueried.temperature == true)
-    {
-        //We've got old data so trigger new measurement
-        triggerMeasurement();
+  // Time out after 100ms
+  int counter = 0;
+  while (isBusy() && counter++ < 100) {
+    delay(1);
+  }
+  if (counter >= 100) {
+    return false;
+  }
 
-        delay(75); //Wait for measurement to complete
+  // Reset timer/flags
+  _last_read = millis();
+  _measurementStarted = false;
 
-        uint8_t counter = 0;
-        while (isBusy())
-        {
-            delay(1);
-            if (counter++ > 100)
-                return (false); //Give up after 100ms
-        }
+  // Read new data
+  if (_Wire->requestFrom(_deviceAddress, (uint8_t)7) != 7) {
+    return false;
+  }
 
-        readData();
-    }
+  uint8_t rx_byte;
+  crc.reset();
 
-    //From datasheet pg 8
-    float tempCelsius = ((float)sensorData.temperature / 1048576) * 200 - 50;
+  // Read status
+  rx_byte = _Wire->read();
+  crc.update(rx_byte);
 
-    //Mark data as old
-    sensorQueried.temperature = true;
+  // Read humidity
+  uint32_t humidity = 0;
+  for (int i = 2; i >= 0; i--) {
+    rx_byte = _Wire->read();
+    crc.update(rx_byte);
+    humidity |= (uint32_t)rx_byte << (8 * i);
+  }
+  humidity >>= 4;
 
-    return tempCelsius;
+  // Read temperature
+  uint32_t temperature = (uint32_t)rx_byte << 16;
+  for (int i = 1; i >= 0; i--) {
+    rx_byte = _Wire->read();
+    crc.update(rx_byte);
+    temperature |= (uint32_t)rx_byte << (8 * i);
+  }
+  temperature &= ~0xFFF00000;
+
+  // Read and check CRC
+  crc.update(_Wire->read());
+  if (crc.crc != 0) {
+    return false;
+  }
+
+  // Save data only if it's valid
+  sensorData.temperature = temperature;
+  sensorData.humidity = humidity;
+
+  // Mark data as fresh
+  sensorQueried.temperature = false;
+  sensorQueried.humidity = false;
+  return true;
 }
 
-float AHT20::getHumidity()
-{
-    if (sensorQueried.humidity == true)
-    {
-        //We've got old data so trigger new measurement
-        triggerMeasurement();
+// Return true if both datums are fresh
+// Return false otherwise
+// If either datum is old, start a new reading
+bool AHT20::available() {
+  // Both datums are fresh
+  if (!sensorQueried.humidity && !sensorQueried.temperature) {
+    return true;
+  }
 
-        delay(75); //Wait for measurement to complete
+  // Trigger a measurement if one has not been previously started, 
+  // then return false
+  if (!_measurementStarted) {
+    if (triggerMeasurement()) delay(75);
+    return false;
+  }
 
-        uint8_t counter = 0;
-        while (isBusy())
-        {
-            delay(1);
-            if (counter++ > 100)
-                return (false); //Give up after 100ms
-        }
+  // If measurement has been started, check to see if it's complete.
+  // If not complete, return false
+  if (isBusy()) {
+    return false;
+  }
 
-        readData();
+  // If complete, read data
+  // Automatically restart measurement if read fails
+  if (!readData()) {
+    if (triggerMeasurement()) delay(75);
+    return false;
+  }
+  return true;
+}
+
+// Get temperature value in celsius
+float AHT20::getTemperature(bool force_new) {
+  // If data is old, start new measurement
+  // If new data isn't readily available, use old data (unless force_new is true)
+  while (sensorQueried.temperature) {
+    readData();
+    if (!force_new) {
+      break;
     }
+    delay(1);
+  }
 
-    //From datasheet pg 8
-    float relHumidity = ((float)sensorData.humidity / 1048576) * 100;
+  // Convert raw value to celsius
+  float tempCelsius = ((float)sensorData.temperature / 1048576) * 200 - 50;
 
-    //Mark data as old
-    sensorQueried.humidity = true;
+  // Mark data as old
+  sensorQueried.temperature = true;
 
-    return relHumidity;
+  return tempCelsius;
+}
+
+// Get humidity value as a percent [0-100]
+float AHT20::getHumidity(bool force_new) {
+  // If data is old, get new data
+  // If new data isn't readily available, use old data (unless force_new is true)
+  while (sensorQueried.humidity) {
+    readData();
+    if (!force_new) {
+      break;
+    }
+    delay(1);
+  }
+
+  // Convert raw value to percent
+  float relHumidity = ((float)sensorData.humidity / 1048576) * 100;
+
+  // Mark data as old
+  sensorQueried.humidity = true;
+
+  return relHumidity;
 }
--- src/SparkFun_Qwiic_Humidity_AHT20.h
+++ src/SparkFun_Qwiic_Humidity_AHT20.h
@@ -1,75 +1,84 @@
-/****************************************************************
- * SparkFun_Qwiic_Humidity_AHT20.h
- * SparkFun Qwiic Humidity AHT20 Library Header File
- * Priyanka Makin @ SparkFun Electronics
- * Original Creation Date: March 25, 2020
- * https://github.com/sparkfun/SparkFun_Qwiic_Humidity_AHT20_Arduino_Library
- * 
- * Pickup a board here: https://wwww.sparkfun.com/products/16618
- * 
- * This file prototypes the AHT20 class implemented in SparkFun_Qwiic_Humidity_AHT20.cpp
- * 
- * Development environment specifics:
- *  IDE: Arduino 1.8.9
- *  Hardware Platform: Arduino Uno
- *  Qwiic Humidity AHT20 Version: 1.0.0
- * 
- * This code is lemonade-ware; if you see me (or any other SparkFun employee)
- * at the local, and you've found our code helpful, please buy us a round!
- * 
- ******************************************************************/
-
-#ifndef __SparkFun_Qwiic_Humidity_AHT20_H__
-#define __SparkFun_Qwiic_Humidity_AHT20_H__
+#ifndef AHT20_H
+#define AHT20_H
 
 #include <Arduino.h>
 #include <Wire.h>
 
 #define AHT20_DEFAULT_ADDRESS 0x38
 
-enum registers
-{
-    sfe_aht20_reg_reset = 0xBA,
-    sfe_aht20_reg_initialize = 0xBE,
-    sfe_aht20_reg_measure = 0xAC,
+// Reset Command
+#define CMD_RESET 0xBA
+
+// Initialization sequence 0xBE0800
+#define CMD_INITIALIZE_2 0xBE
+#define CMD_INITIALIZE_1 0x08
+#define CMD_INITIALIZE_0 0x00
+
+// Measurment sequence 0xAC3300
+#define CMD_MEASURE_2 0xAC
+#define CMD_MEASURE_1 0x33
+#define CMD_MEASURE_0 0x00
+
+
+// Class to handle cyclic redundancy checking
+struct crc8 {
+  uint8_t crc = 0xFF;
+
+  void reset() { crc = 0xFF; }
+  void update(uint8_t data) {
+    crc = crc ^ data;
+    for (int i = 0; i < 8; i++) {
+      if (crc & 0x80)
+        crc = (crc << 1) ^ 0x31;
+      else
+        crc <<= 1;
+    }
+  }
 };
 
-class AHT20
-{
-private:
-    TwoWire *_i2cPort; //The generic connection to user's chosen I2C hardware
-    uint8_t _deviceAddress;
-    bool measurementStarted = false;
 
-    struct
-    {
-        uint32_t humidity;
-        uint32_t temperature;
-    } sensorData;
+class AHT20 {
+ private:
+  // I2C variables
+  TwoWire* _Wire;
+  uint8_t _deviceAddress;
+  crc8 crc;
 
-    struct
-    {
-        uint8_t temperature : 1;
-        uint8_t humidity : 1;
-    } sensorQueried;
+  // Measurement variables
+  uint32_t _read_delay;
+  uint32_t _last_read;
+  bool _measurementStarted = false;
 
-public:
-    //Device status
-    bool begin(TwoWire &wirePort = Wire); //Sets the address of the device and opens the I2C bus
-    bool isConnected();                   //Checks if the AHT20 is connected to the I2C bus
-    bool available();                     //Returns true if new data is available
+  struct {
+    uint32_t humidity;
+    uint32_t temperature;
+  } sensorData;
 
-    //Measurement helper functions
-    uint8_t getStatus();       //Returns the status byte
-    bool isCalibrated();       //Returns true if the cal bit is set, false otherwise
-    bool isBusy();             //Returns true if the busy bit is set, false otherwise
-    bool initialize();         //Initialize for taking measurement
-    bool triggerMeasurement(); //Trigger the AHT20 to take a measurement
-    void readData();           //Read and parse the 6 bytes of data into raw humidity and temp
-    bool softReset();          //Restart the sensor system without turning power off and on
+  struct {
+    uint8_t temperature : 1;
+    uint8_t humidity : 1;
+  } sensorQueried;
 
-    //Make measurements
-    float getTemperature(); //Goes through the measurement sequence and returns temperature in degrees celcius
-    float getHumidity();    //Goes through the measurement sequence and returns humidity in % RH
+ public:
+  explicit AHT20(const uint8_t deviceAddress = AHT20_DEFAULT_ADDRESS);  // Sets the address of the device
+  bool begin(TwoWire* wire = &Wire, uint32_t read_delay_ms = 2000);
+
+  // Device status
+  uint8_t getStatus();  // Returns the status byte
+  bool available();     // Returns true if new data is available
+  bool isConnected();   // Checks if the AHT20 is connected to the I2C bus
+  bool isCalibrated();  // Returns true if the cal bit is set, false otherwise
+  bool isBusy();        // Returns true if the busy bit is set, false otherwise
+
+  // Measurement helper functions
+  bool initialize();          // Initialize for taking measurement
+  bool triggerMeasurement();  // Trigger the AHT20 to take a measurement
+  bool readData();  // Read and parse the 6 bytes of data into raw humidity and temp
+  bool softReset();  // Restart the sensor system without turning power off and on
+
+  // Getter functions
+  float getTemperature(bool force_new = true);  // Goes through the measurement sequence and returns temperature in degrees celsius
+  float getHumidity(bool force_new = true);  // Goes through the measurement sequence and returns humidity in % RH
 };
+
 #endif
